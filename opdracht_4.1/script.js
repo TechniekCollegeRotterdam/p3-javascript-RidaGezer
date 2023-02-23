ae0e33ff75cc07c5667d51a45c90905cf82477d3
ridagezer <9013814@student.zadkine.nl>
2023-02-23 10:48:03 +0100

    4.1

--- opdracht_4.1/script.js
+++ opdracht_4.1/script.js
@@ -4,17 +4,17 @@ let input2 = document.getElementById("input2")
 
 
 function multiply(){
-    answer.innerText = input1.value * input2.value;
+    answer.innerText = Number(input1.value) * Number(input2.value);
 }
 
 function add(){
-     answer.innerText = input1.value + input2.value;
+     answer.innerText = Number(input1.value) + Number(input2.value);
 }
 
 function minus(){
-    answer.innerText = input1.value - input2.value;
+    answer.innerText = Number(input1.value) - Number(input2.value);
 }
 
 function divide(){
-    answer.innerText = input1.value / input2.value;
+    answer.innerText = Number(input1.value) / Number(input2.value);
 }
